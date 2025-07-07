05506a01bf5b9434b85c28f63c8ec3b5de92f428
Febriananta Rafa Fadilah <febrianantarafa@apps.ipb.ac.id>
2025-07-07 17:22:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import altair as alt
 from itertools import product
 
 # === CONFIG PAGE ===
-st.set_page_config(layout="wide", page_title="Dashboard PJK MPKMB IPB 62")
+st.set_page_config(layout="wide", page_title="Dashboard PJK Sarjana MPKMB IPB 62")
 
 @st.cache_data(show_spinner=False)
 def load_data():
